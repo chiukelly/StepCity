ccfb070e48bec26aa271e2c7bf537e244fc22e67
UgochukwuNmz <68568717+UgochukwuNmz@users.noreply.github.com>
2020-11-07 14:37:10 -0600

    Made project

---
+++ StepCity/StepCityApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCityApp.swift
+//  StepCity
+//
+//  Created by Emmanuel Ihim on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct StepCityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
