ad6017d68043abd23780fa39fbb6f817c2e2d5cf
UgochukwuNmz <68568717+UgochukwuNmz@users.noreply.github.com>
2020-11-07 16:37:27 -0600

    added firebase integration

---
+++ StepCity/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  StepCity
+//
+//  Created by Emmanuel Ihim on 11/7/20.
+//
+
+import Foundation
+import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        /// your push notification code goes here
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct HelloWorldApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
