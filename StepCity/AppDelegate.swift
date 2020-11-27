e10a3291b755ebeb917cb2c862be9441969d4f0d
Kelly Chiu <chiukelly3@gmail.com>
2020-11-27 16:10:54 -0600

    app delegate

--- StepCity/AppDelegate.swift
+++ StepCity/AppDelegate.swift
@@ -8,7 +8,7 @@
 //import Foundation
 //import SwiftUI
 //import Firebase
-
+//
 //class AppDelegate: NSObject, UIApplicationDelegate {
 //    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 //        FirebaseApp.configure()
@@ -26,3 +26,22 @@
 //        }
 //    }
 //}
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        
+    }
+}
