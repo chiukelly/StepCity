587d829bdce08060a39530db49b97de33774e099
UgochukwuNmz <68568717+UgochukwuNmz@users.noreply.github.com>
2020-11-30 11:15:55 -0600

    save

--- StepCity/AuthView.swift
+++ StepCity/AuthView.swift
@@ -32,33 +32,52 @@ struct SignInView : View {
 
     var body: some View {
         GeometryReader { gr in
-            VStack {
+            VStack (spacing: 18.0){
             // App name
             LinearGradient(gradient: Gradient(colors: [.black, Color(#colorLiteral(red: 0.16862745583057404, green: 0.22745098173618317, blue: 0.843137264251709, alpha: 1))]),
             startPoint: .leading,endPoint: .trailing)
-            .mask(Text("STEPCITY")).font(Font.custom("PixelOperator-Bold", size: gr.size.height * 0.10))
-            .frame(width: gr.size.width, height: gr.size.height * 0.30)
+                .mask(Text("STEPCITY"))
+                .padding(.bottom)
+                .font(Font.custom("PixelOperator-Bold", size: gr.size.height * 0.10))
+                .frame(width: gr.size.width, height: gr.size.height * 0.10)
                 
             // email field
-            TextField("enter email", text: $email).padding().background(lightGrayColor).cornerRadius(40.0).padding(.bottom, 20).frame(width: gr.size.width * 0.90)
+            TextField("enter email", text: $email)
+                .padding()
+                .background(lightGrayColor)
+                .cornerRadius(40.0)
+                .frame(width: gr.size.width * 0.90)
                 
             // password field
-            SecureField("enter password", text: $password).padding().background(lightGrayColor).cornerRadius(50.0).padding(.bottom, 20).frame(width: gr.size.width * 0.90)
+            SecureField("enter password", text: $password)
+                .padding()
+                .background(lightGrayColor)
+                .cornerRadius(50.0)
+                .frame(width: gr.size.width * 0.90)
                 
             // login button
             Button(action: {print("Button Tapped")}) {
-            Text("sign in").font(Font.custom("Roboto-Light", size: gr.size.height * 0.023)).frame(width: gr.size.width * 0.90, height: gr.size.height * 0.05).foregroundColor(.white).background(darkBlueColor).cornerRadius(50.0)
+                Text("sign in")
+                    .font(Font.custom("Roboto-Light", size: gr.size.height * 0.023))
+                    .frame(width: gr.size.width * 0.90, height: gr.size.height * 0.04)
+                    .foregroundColor(.white).background(darkBlueColor).cornerRadius(50.0)
             }
                 
             if (error != "") {
-            Text(error).font(Font.custom("Roboto-Light", size: gr.size.height * 0.023)).foregroundColor(.red)
+                Text(error)
+                    .font(Font.custom("Roboto-Light", size: gr.size.height * 0.023))
+                    .foregroundColor(.red)
             }
                 
             NavigationLink(destination: SignUpView()) {
                 HStack {
-                    Text("don't have an account?").font(Font.custom("Roboto-Light", size: gr.size.height * 0.023)).foregroundColor(.black)
+                    Text("don't have an account?")
+                        .font(Font.custom("Roboto-Light", size: gr.size.height * 0.023))
+                        .foregroundColor(.black)
                     
-                    Text("sign up").font(Font.custom("Roboto-Light", size: gr.size.height * 0.023)).foregroundColor(darkBlueColor)
+                    Text("sign up")
+                        .font(Font.custom("Roboto-Light", size: gr.size.height * 0.023))
+                        .foregroundColor(darkBlueColor)
                     }
                 }
             }
@@ -91,27 +110,53 @@ struct SignUpView: View {
         GeometryReader { gr in
             VStack {
                 // create account text
-                Text("CREATE ACCOUNT").font(Font.custom("PixelOperator-Bold", size: gr.size.height * 0.10)).foregroundColor(.black)
+                Text("CREATE ACCOUNT")
+                    .font(Font.custom("PixelOperator-Bold", size: gr.size.height * 0.10))
+                    .foregroundColor(.black)
                 
                 // email field
-                TextField("enter email", text: $email).padding().background(lightGrayColor).cornerRadius(40.0).padding(.bottom, 20).frame(width: gr.size.width * 0.90)
+                TextField("enter email", text: $email)
+                    .padding()
+                    .background(lightGrayColor)
+                    .cornerRadius(40.0)
+                    .padding(.bottom, 20)
+                    .frame(width: gr.size.width * 0.90)
                 
                 // full name field
-                SecureField("enter full name", text: $password).padding().background(lightGrayColor).cornerRadius(50.0).padding(.bottom, 20).frame(width: gr.size.width * 0.90)
+                SecureField("enter full name", text: $password)
+                    .padding()
+                    .background(lightGrayColor)
+                    .cornerRadius(50.0).padding(.bottom, 20)
+                    .frame(width: gr.size.width * 0.90)
                 
                 // username field
-                TextField("enter username", text: $password).padding().background(lightGrayColor).cornerRadius(50.0).padding(.bottom, 20).frame(width: gr.size.width * 0.90)
+                TextField("enter username", text: $password)
+                    .padding()
+                    .background(lightGrayColor)
+                    .cornerRadius(50.0)
+                    .padding(.bottom, 20)
+                    .frame(width: gr.size.width * 0.90)
                 
                 // password field
-                TextField("enter password", text: $password).padding().background(lightGrayColor).cornerRadius(50.0).padding(.bottom, 20).frame(width: gr.size.width * 0.90)
+                TextField("enter password", text: $password)
+                    .padding()
+                    .background(lightGrayColor)
+                    .cornerRadius(50.0)
+                    .padding(.bottom, 20)
+                    .frame(width: gr.size.width * 0.90)
                 
                 // sign up button
                 Button(action: signUp) {
-                Text("sign up").font(Font.custom("Roboto-Light", size: gr.size.height * 0.023)).frame(width: gr.size.width * 0.90, height: gr.size.height * 0.05).foregroundColor(.white).background(darkBlueColor).cornerRadius(50.0)
+                Text("sign up")
+                    .font(Font.custom("Roboto-Light", size: gr.size.height * 0.023))
+                    .frame(width: gr.size.width * 0.90, height: gr.size.height * 0.05)
+                    .foregroundColor(.white).background(darkBlueColor).cornerRadius(50.0)
                 }
                     
                 if (error != "") {
-                    Text(error).font(Font.custom("Roboto-Light", size: gr.size.height * 0.023)).foregroundColor(.red)
+                    Text(error)
+                        .font(Font.custom("Roboto-Light", size: gr.size.height * 0.023))
+                        .foregroundColor(.red)
                 }
             }
         }
@@ -122,7 +167,7 @@ struct SignUpView: View {
 struct AuthView: View {
     var body: some View {
         NavigationView {
-            SignUpView()
+            SignInView()
         }
     }
 }
