5b0c7193f522b100f0cb311cbc03df5fef5a4e51
Kelly Chiu <chiukelly3@gmail.com>
2020-11-30 10:51:30 -0600

    stepcity fixed merge

--- StepCity/StepCityApp.swift
+++ StepCity/StepCityApp.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Emmanuel Ihim on 11/7/20.
 //
-<<<<<<< HEAD
 
 import SwiftUI
 
@@ -12,9 +11,9 @@ import SwiftUI
 struct StepCityApp: App {
     var body: some Scene {
         WindowGroup {
-            BottomNavigationBar()
+            Leaderboard()
         }
-    }  
+    }
 }
 
 struct StepCityApp_Previews: PreviewProvider {
@@ -22,22 +21,3 @@ struct StepCityApp_Previews: PreviewProvider {
         /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
-=======
-//
-//import SwiftUI
-//
-//@main
-//struct StepCityApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            Leaderboard()
-//        }
-//    }
-//}
-//
-//struct StepCityApp_Previews: PreviewProvider {
-//    static var previews: some View {
-//        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
-//    }
-//}
->>>>>>> 4e0a485e8312f0a11ad2a1066426e60cd49dec80
